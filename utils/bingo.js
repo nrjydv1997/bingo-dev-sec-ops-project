f2e5a2c703b9974ad6ad519ba7f8b8ce2c5069fd
nrjydv1997 <nrjydv1997@gmail.com>
2026-05-07 16:40:46 +0530

    bug removed

--- utils/bingo.js
+++ utils/bingo.js
@@ -1,21 +1,20 @@
 export const bingo = (amount) => {
-  let numbers = [];
-  let riffledNumbers = [];
+  const numbers = [];
+  const riffledNumbers = [];
 
-  for (let i = 1; i <= amount; i++) numbers.push(i);
-
-  function generateRandomInteger(max) {
-    return Math.floor(Math.random() * max);
+  for (let i = 1; i <= amount; i++) {
+    numbers.push(i);
   }
 
-  let tt = true;
+  const generateRandomInteger = (max) => {
+    return Math.floor(Math.random() * max);
+  };
 
-  while (tt) {
-    let riffled = generateRandomInteger(numbers.length);
+  while (riffledNumbers.length < amount) {
+    const riffled = generateRandomInteger(numbers.length);
+
     riffledNumbers.push(numbers[riffled]);
-
     numbers.splice(riffled, 1);
-    tt = riffledNumbers.length >= amount ? false : true;
   }
 
   return riffledNumbers;
@@ -25,35 +24,49 @@ export const createCartela = (amount, cartelas) => {
   let numbers;
   let cartela;
 
+  const generateRandomInteger = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
   do {
     numbers = [];
     cartela = [];
 
-    for (let i = 1; i <= amount; i++) numbers.push(i);
-
-    function generateRandomInteger(max) {
-      return Math.floor(Math.random() * max);
+    for (let i = 1; i <= amount; i++) {
+      numbers.push(i);
     }
 
     let cartelaAmount;
+
     switch (generateRandomInteger(3)) {
       case 0:
         cartelaAmount = 16;
         break;
+
       case 1:
         cartelaAmount = 17;
         break;
+
       case 2:
         cartelaAmount = 18;
         break;
+
+      default:
+        cartelaAmount = 16;
     }
 
     for (let i = 1; i <= cartelaAmount; i++) {
-      let riffled = generateRandomInteger(numbers.length);
+      const riffled = generateRandomInteger(numbers.length);
+
       cartela.push(numbers[riffled]);
       numbers.splice(riffled, 1);
     }
-  } while (typeof cartelas.find((el) => el == cartela) == undefined);
+
+  } while (
+    cartelas.find(
+      (el) => JSON.stringify(el) === JSON.stringify(cartela)
+    ) !== undefined
+  );
 
   return cartela;
 };
